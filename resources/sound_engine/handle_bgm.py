21935746adc6c3d53506b3a2257bdc190891dc71
chumpatrol1 <chumpatrol1@gmail.com>
2021-09-30 09:02:01 -0400

    Updated Sound Engine
    Added a lot of todos
    
    Changed the way that music is loaded and looped in order to help combat lag
    
    Added bgm class to handle_bgm.py, and bundled everything together. Songs are now selected randomly from a list.

--- resources/sound_engine/handle_bgm.py
+++ resources/sound_engine/handle_bgm.py
@@ -1,16 +1,25 @@
 from os import getcwd
-cwd = getcwd()
+from random import randint
+cwd = getcwd() + '/resources/sounds/bgm/'
+# TODO: Add more songs for tests
+# TODO: Make documentation
+
+class bgm:
+    def __init__(self, track_file = "Blob_Ball_Main_Theme.wav", track_name = "Blob Ball Main Theme", track_number = 1, track_duration = 133.5, restart_point = 10):
+        self.track_file = cwd + track_file
+        self.track_name = track_name
+        self.track_number = track_number
+        self.track_duration = track_duration
+        self.restart_point = restart_point
+
+    def __str__(self):
+        return "Track Name (Number): ({}) {}, Track Duration: {}, Restart Point: {}".format(self.track_number, self.track_name, self.track_duration, self.restart_point)
 
 bgm_list = {
-    "bb_main_theme": cwd + "/resources/sounds/bgm/Blob_Ball_Main_Theme.wav"
-}
-
-bgm_duration = { # How many frames does this song last before looping?
-    "bb_main_theme": 7500
+    "bb_main_theme": [bgm(track_file="Blob_Ball_Main_Theme.wav", track_name = "Blob Ball Main Theme", track_number=1, track_duration=133.5, restart_point=10)]
 }
 
 def load_bgm(song_playing):
-    return bgm_list[song_playing]
-
-def get_bgm_duration(song_playing):
-    return bgm_duration[song_playing]
+    track_array = bgm_list[song_playing]
+    random_track = randint(0, len(track_array) - 1)
+    return track_array[random_track]
--- resources/sound_engine/handle_sound.py
+++ resources/sound_engine/handle_sound.py
@@ -1,6 +1,6 @@
 import pygame as pg
-from pygame.constants import BLEND_RGBA_MULT
-from resources.sound_engine.handle_bgm import load_bgm, get_bgm_duration
+from resources.sound_engine.handle_bgm import load_bgm
+from time import time
 pg.init()
 pg.mixer.init()
 print("PYGAME SOUND:", pg.mixer.get_init())
@@ -8,17 +8,33 @@ print("PYGAME SOUND:", pg.mixer.get_init())
 saved_song = ""
 bgm = None
 bgm_timer = 0
+bgm_class = None
+start_time = 0
+elapsed_time = 0
+
+# TODO: Make handle_bgm.py actually play the music? At least move this to a separate function
 
 def handle_sound(song_playing):
     global saved_song
     global bgm_timer
+    global start_time
+    global bgm_class
+
     # Play BGM
     # Play SFX
     if(song_playing != saved_song) or not bgm_timer:
         saved_song = song_playing
-        pg.mixer.music.load(load_bgm(song_playing))
-    if not bgm_timer:
+        bgm_class = load_bgm(song_playing)
+        bgm_timer = bgm_class.track_duration
+        start_time = time()
+        pg.mixer.music.load(bgm_class.track_file)
         pg.mixer.music.play(-1)
-        bgm_timer = get_bgm_duration(song_playing)
-    else:
-        bgm_timer -= 1
+    elapsed_time = time() - start_time
+
+    if elapsed_time >= bgm_timer:
+        start_time = time()
+        try:
+            pg.mixer.music.play(-1, start = bgm_class.restart_point)
+        except pg.error as message:
+            print("handle_sound.py error:", message)
+            pg.mixer.music.play(-1)
