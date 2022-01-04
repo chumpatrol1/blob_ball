2e254a020db9e0e47b745a5a4d05afc2b9deb4dd
chumpatrol1 <chumpatrol1@gmail.com>
2022-01-04 08:49:59 -0500

    Testing Crash Bug

--- resources/graphics_engine/display_main_menu.py
+++ resources/graphics_engine/display_main_menu.py
@@ -3,11 +3,18 @@ import pygame as pg
 from os import getcwd
 cwd = getcwd()
 
-def draw_main_menu(game_display, info_getter, settings):
-    selector_position = info_getter[0]
-    ruleset = info_getter[1]
-    draw_background(game_display, 'main_menu', settings)
-    menu_font = pg.font.Font(cwd + "/resources/fonts/neuropol-x-free.regular.ttf", 40)
+menu_asset_cache = {
+'ball': pg.transform.scale(pg.image.load(cwd + "/resources/images/balls/soccer_ball.png"), (76, 76)),
+'initialized': False,
+}
+
+def draw_main_menu(game_display, info_getter, settings):    
+    if not menu_asset_cache['initialized']:
+        menu_asset_cache['font'] = pg.font.Font(cwd + "/resources/fonts/neuropol-x-free.regular.ttf", 40)
+        menu_asset_cache['initialized'] = True
+
+
+    menu_font = menu_asset_cache['font']
     text_array = [
         menu_font.render('Play!', False, (0, 0, 150)),
         menu_font.render('Online', False, (0, 0, 150)),
@@ -17,9 +24,11 @@ def draw_main_menu(game_display, info_getter, settings):
         menu_font.render('Quit', False, (0, 0, 150))
     ]
 
+    selector_position = info_getter[0]
+    ruleset = info_getter[1]
+    draw_background(game_display, 'main_menu', settings)
 
-    ball = pg.image.load(cwd + "/resources/images/balls/soccer_ball.png")
-    ball = pg.transform.scale(ball, (76, 76))
+    ball = menu_asset_cache['ball']
     game_display.blit(ball, (850, (76 * selector_position) + 38))
 
     text_y = 76
@@ -34,3 +43,4 @@ def draw_main_menu(game_display, info_getter, settings):
     text_rect = text_box.get_rect()
     text_rect.bottomright = (1366, 768)
     game_display.blit(text_box, text_rect)
+
