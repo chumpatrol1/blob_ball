aeadbb35de47b4f43cf2e287814b9b776a12ca6c
chumpatrol1 <chumpatrol1@gmail.com>
2022-05-01 07:05:42 -0400

    Update to Proto 02

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function download() {
-    window.location.replace('https://github.com/chumpatrol1/blob_ball/releases/tag/v0.13.0b');
+    window.location.replace('https://github.com/chumpatrol1/blob_ball/releases/tag/v0.14.0b');
+}
+function youtube() {
+	window.location.replace('https://www.youtube.com/watch?v=haoTuA2hMVc&t=297s'); //temp
+}
+function discord() {
+	window.location.replace('https://discord.gg/sPR2B9hhkF');
 }
