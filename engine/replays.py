5db29674dc1de49d271b2abd19815ac7a61eac3a
chumpatrol1 <chumpatrol1@gmail.com>
2022-08-23 17:51:54 -0400

    Fixed Replays again(?) (#43)
    Fixed a crash caused by unlocks.py
    
    Replays are now saved using json structures rather than lots of string appends, and empty string is placed at the end (rather than the beginning) of the replay list

--- engine/replays.py
+++ engine/replays.py
@@ -7,7 +7,9 @@ import zlib
 from resources.graphics_engine.display_controller_pop_up import create_generic_pop_up
 
 def compress_replay_file(string_to_compress, file_str):
-    compressed_data = zlib.compress(string_to_compress.encode('ascii'), zlib.Z_BEST_COMPRESSION)
+    full_string = string_to_compress
+    #print(full_string)
+    compressed_data = zlib.compress(full_string.encode('ascii'), zlib.Z_BEST_COMPRESSION)
     with open(file_str, "wb") as compressed_v:
         compressed_v.write(compressed_data)
     '''with open("bruh.txt", "w") as compressed_v:
@@ -17,21 +19,25 @@ def compress_replay_file(string_to_compress, file_str):
 current_replay = None
 
 def check_replay_integrity(current_replay):
-    if(len(current_replay[4]) != current_replay[5]['time']):
+    # Compare the inputs we stored to the amount of time shown in the gameplay
+    #print(len(current_replay[6]), current_replay[7]['time'] + 1)
+    if(len(current_replay[6]) != current_replay[7]['time'] + 1):
         create_generic_pop_up(-1)
 
 def decompress_replay_file(file_name): # DANGER: DO NOT USE! WE NEED TO HAVE FILE EXPLORER
     global current_replay
     with open(file_name, "rb") as compressed_v:
         string_to_compress = compressed_v.read()
-    decompressed_data = zlib.decompress(string_to_compress).decode('ascii').split('\n')
+    decompressed_data = zlib.decompress(string_to_compress).decode('ascii')
     #print('seed', decompressed_data[0])
     #print('rules', loads(decompressed_data[1]))
     #print('p1', loads(decompressed_data[2])['species'])
     #print('p2', loads(decompressed_data[2])['species'])
-
-    current_replay = [decompressed_data[0], loads(decompressed_data[1]), loads(decompressed_data[2])['species'], loads(decompressed_data[2])['costume'], loads(decompressed_data[3])['species'], loads(decompressed_data[3])['costume'], loads(decompressed_data[4]), loads(decompressed_data[5])]
-
+    #print(type(decompressed_data))
+    #print(decompressed_data)
+    loaded_replay = loads(decompressed_data)
+    current_replay = [loaded_replay[0], loaded_replay[1], loaded_replay[2]['species'], loaded_replay[2]['costume'], loaded_replay[3]['species'], loaded_replay[3]['costume'], loaded_replay[4], loaded_replay[5]]
+    print(current_replay[7])
     check_replay_integrity(current_replay)
     #print(decompressed_data[4].split('/'))
     #print(decompressed_data)
@@ -52,6 +58,5 @@ def save_replay(random_seed, ruleset, replay_inputs, p1_blob, p2_blob, game_info
     while exists(file_str + ".bbr"):
         identifier += 1
         file_str = getenv('APPDATA')+'/BlobBall' + '/replays/' + p1_blob.species.capitalize() + " vs " + p2_blob.species.capitalize() + " " + time_str + str(identifier) + ".bbr"
-
-    string_to_save = str(random_seed) + "\n" + dumps(ruleset) + "\n" + dumps(p1_blob.info) + "\n" + dumps(p2_blob.info) + "\n" + dumps(replay_inputs) + "\n" + dumps(game_info)
-    compress_replay_file(string_to_save, file_str)
+    json_to_save = dumps([str(random_seed), ruleset, p1_blob.info, p2_blob.info, replay_inputs, game_info])
+    compress_replay_file(json_to_save, file_str)
